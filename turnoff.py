b7a9be6dc59e9aa2eadeb426586813241ea6683c
GlowingGaijin <130335593+GlowingGaijin@users.noreply.github.com>
2023-04-22 22:19:29 -0700

    Update turnoff.py

--- turnoff.py
+++ turnoff.py
@@ -12,10 +12,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.OUT)  # Set up pin 5 as an output pin
 
 while True:
-    key_press = input("Press 'g' to turn off output pin 5, or 'w' to delete known_networks.txt file: ")
+    key_press = input("Press 'g' to turn off output pin 5, or 'w' to delete known_macs.txt file: ")
     
     if key_press == "g":
         GPIO.output(5, GPIO.LOW)  # Turn off output pin 5
     elif key_press == "w":
-        with open("known_networks.txt", "w") as f:
+        with open("known_macs.txt", "w") as f:
             f.write("")  # Write an empty string to the file to clear its contents
